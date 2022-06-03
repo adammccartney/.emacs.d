64e1689955a02fbca28e622af4415b15e162cd95
adammccartney <adam@mur.at>
2022-06-03 11:12:02 +0200

    Add mu4e single account setup
    This contains a working config for mu4e

--- init.el
+++ init.el
@@ -449,4 +449,88 @@
   :init
   (setq org-startup-folded "showall"))
 
+(use-package mu4e
+  :defer 20
+  :config
+
+  ;; Refresh mail using isync every 10 minutes
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq mu4e-maildir "~/.mail")
+
+  ;; Make sure that moving a message (like to Trash) causes the
+  ;; message to get a new file name.  This helps to avoid the
+  ;; dreaded "UID is N beyond highest assigned" error.
+  ;; See this link for more info: https://stackoverflow.com/a/43461973
+  (setq mu4e-change-filenames-when-moving t)
+  (setq mu4e-contexts
+          `(,(make-mu4e-context
+              :name "murat"
+              :match-func (lambda (msg) (when msg
+                                          (string-prefix-p "/murat" (mu4e-message-field msg :maildir))))
+              :vars '(
+                      (user-full-name . "Adam McCartney")
+                      (user-mail-address . "adam@mur.at")
+                      (mu4e-sent-folder . "/Sent")
+                      (mu4e-trash-folder . "/Trash")
+                      (mu4e-drafts-folder . "/Drafts")
+                      (mu4e-refile-folder . "/Archive")
+                      (mu4e-sent-messages-behavior . sent)
+                      ))
+          ))
+  (setq mu4e-context-policy 'pick-first)
+  ;; Prevent mu4e from permanently deleting trashed items
+  ;; This snippet was taken from the following article:
+  ;; http://cachestocaches.com/2017/3/complete-guide-email-emacs-using-mu-and-/
+  (defun remove-nth-element (nth list)
+    (if (zerop nth) (cdr list)
+      (let ((last (nthcdr (1- nth) list)))
+        (setcdr last (cddr last))
+        list)))
+  (setq mu4e-marks (remove-nth-element 5 mu4e-marks))
+  (add-to-list 'mu4e-marks
+               '(trash
+                 :char ("d" . "▼")
+                 :prompt "dtrash"
+                 :dyn-target (lambda (target msg) (mu4e-get-trash-folder msg))
+                 :action (lambda (docid msg target)
+                           (mu4e~proc-move docid
+                                           (mu4e~mark-check-target target) "-N"))))
+
+  ;; Display options
+  (setq mu4e-view-show-images t)
+  (setq mu4e-view-show-addresses 't)
+
+  ;; Composing mail
+  (setq mu4e-compose-dont-reply-to-self t)
+
+  ;; Use mu4e for sending e-mail
+  (setq mail-user-agent 'mu4e-user-agent
+        message-send-mail-function 'smtpmail-send-it
+        smtpmail-smtp-server "smtp.mur.at"
+        smtpmail-smtp-service 587)
+
+;; setup some handy shortcuts
+  ;; you can quickly switch to your Inbox -- press ``ji''
+  ;; then, when you want archive some messages, move them to
+  ;; the 'All Mail' folder by pressing ``ma''.
+  (setq mu4e-maildir-shortcuts
+        '(("/Inbox"  . ?i)
+          ("/Drafts" . ?l)
+          ("/Sent"   . ?s)
+          ("/Trash"  . ?t)))
+
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name "All Inboxes"
+                :query "maildir:/murat/INBOX"
+                :key ?i))
+
+  ;; don't keep message buffers around
+  (setq message-kill-buffer-on-exit t)
+
+  ;; Start mu4e in the background so that it syncs mail periodically
+  (mu4e t))
+
+
 (provide 'init) ; make (require 'init) happy
